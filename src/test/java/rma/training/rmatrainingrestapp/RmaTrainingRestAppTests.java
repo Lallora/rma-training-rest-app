2397aa6bb22c0ad934fb4116eb670151037c9c39
Rafik <lallora@mail.ru>
2023-04-22 15:43:45 +0400

    prototype

---
+++ src/test/java/rma/training/rmatrainingrestapp/RmaTrainingRestAppTests.java
@@ -0,0 +1,13 @@
+package rma.training.rmatrainingrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmaTrainingRestAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
